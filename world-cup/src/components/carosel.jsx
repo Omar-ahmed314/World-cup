f248e30db598a552b3fd6c3731ae6628761241e2
abbas <62177195+Omar-ahmed314@users.noreply.github.com>
2022-12-24 23:01:01 +0200

    add carosel images

--- world-cup/src/components/carosel.jsx
+++ world-cup/src/components/carosel.jsx
@@ -1,11 +1,14 @@
 import '../styles/carosel.css'
+import c1 from '../images/c1.jpg'
+import c2 from '../images/c2.jpeg'
+import c3 from '../images/c3.jpg'
 
 function Carosel() {
   return (
     <div className="caroselContainer">
-        <div>messi</div>
-        <div>ronaldo</div>
-        <div>neymar</div>
+        <div className='c1'><img src={c1}/></div>
+        <div className='c2'><img src={c2}/></div>
+        <div className='c3'><img src={c3}/></div>
     </div>
   );
 }

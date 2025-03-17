e3cd53a899798f9e754dc27779ed7e30953f138d
abbas <62177195+Omar-ahmed314@users.noreply.github.com>
2025-03-17 16:56:16 +0200

    feat: add admin auth into stadium handler

--- Back-end/src/handlers/stadiumHandler.ts
+++ Back-end/src/handlers/stadiumHandler.ts
@@ -1,96 +1,109 @@
-import Stadium, {stadium} from "../models/stadium";
+import Stadium, { stadium } from '../models/stadium';
 import { Response, Request, Application } from 'express';
-import { tokenVerfication } from "./middleware/userHandlerMid";
-import upload from "./middleware/multer";
-import dotenv from 'dotenv'
-import fs from 'fs'
-import path from "path";
+import {
+  tokenVerfication,
+  adminAuthorization,
+} from './middleware/userHandlerMid';
+import upload from './middleware/multer';
+import dotenv from 'dotenv';
+import fs from 'fs';
+import path from 'path';
 
 dotenv.config();
 
 const stadiumModel = new Stadium();
 
 // endpoints
-const index = async (req : Request, res: Response) => {
-   try {
-       const data = await stadiumModel.index();
-       res.status(200);
-       res.json(data);
-   } catch (error) {
+const index = async (req: Request, res: Response) => {
+  try {
+    const data = await stadiumModel.index();
+    res.status(200);
+    res.json(data);
+  } catch (error) {
     res.status(500);
     res.send(error);
-   }
-}
+  }
+};
 
-const show = async (req : Request, res: Response) => {
-    try {
-        const data = await stadiumModel.show(req.params.id as unknown as number);
-        res.status(200);
-        res.json(data);
-    } catch (error) {
-     res.status(400);
-     res.send(error);
-    }
- }
+const show = async (req: Request, res: Response) => {
+  try {
+    const data = await stadiumModel.show(req.params.id as unknown as number);
+    res.status(200);
+    res.json(data);
+  } catch (error) {
+    res.status(400);
+    res.send(error);
+  }
+};
 
 const create = async (req: Request, res: Response) => {
-    try {
-        const stadiumData: stadium = {
-            stadiumName:req.body.stadiumName,
-            noRows:req.body.noRows,
-            noSeatsPerRow:req.body.noSeatsPerRow,
-            imageURL: `stadiums/${req.file?.filename}`
-        };
+  try {
+    const stadiumData: stadium = {
+      stadiumName: req.body.stadiumName,
+      noRows: req.body.noRows,
+      noSeatsPerRow: req.body.noSeatsPerRow,
+      imageURL: `stadiums/${req.file?.filename}`,
+    };
 
-        const data = await stadiumModel.create(stadiumData);
-        res.status(201);
-        res.json({
-            msg: 'Created Successfully'
-        });
-        
-    } catch (error) {
-        console.log(error);
-        res.status(400);
-        res.send(error);
-    }
-}
+    const data = await stadiumModel.create(stadiumData);
+    res.status(201);
+    res.json({
+      msg: 'Created Successfully',
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400);
+    res.send(error);
+  }
+};
 
 const edit = async (req: Request, res: Response) => {
-    try {
-        const stadiumData: stadium = {
-            stadiumID:req.body.stadiumID,
-            stadiumName:req.body.stadiumName,
-            noRows:req.body.noRows,
-            noSeatsPerRow:req.body.noSeatPerRow,
-            imageURL: req.body.stadiumImage
-        };
-        const data = await stadiumModel.edit(stadiumData);
-        res.status(201);
-        res.json(data);
-    } catch (error) {
-        res.status(400);
-        res.send(error);
-    }
-}
+  try {
+    const stadiumData: stadium = {
+      stadiumID: req.body.stadiumID,
+      stadiumName: req.body.stadiumName,
+      noRows: req.body.noRows,
+      noSeatsPerRow: req.body.noSeatPerRow,
+      imageURL: req.body.stadiumImage,
+    };
+    const data = await stadiumModel.edit(stadiumData);
+    res.status(201);
+    res.json(data);
+  } catch (error) {
+    res.status(400);
+    res.send(error);
+  }
+};
 
 const _delete = async (req: Request, res: Response) => {
-    try {
-        const data = await stadiumModel.delete(req.params.id as unknown as number);
-        fs.unlinkSync(path.join(__dirname, '../../', process.env.STATIC_FILES_LOCATION as string, data.imageURL))
-        res.status(200);
-        res.json(data);
-    } catch (error) {
-        res.status(400);
-        res.send(error);
-    }
-}
+  try {
+    const data = await stadiumModel.delete(req.params.id as unknown as number);
+    fs.unlinkSync(
+      path.join(
+        __dirname,
+        '../../',
+        process.env.STATIC_FILES_LOCATION as string,
+        data.imageURL
+      )
+    );
+    res.status(200);
+    res.json(data);
+  } catch (error) {
+    res.status(400);
+    res.send(error);
+  }
+};
 
 const stadiumRoutes = (app: Application) => {
-    app.get('/stadium', index);
-    app.get('/stadium/:id', show);
-    app.post('/stadium', [upload.single('stadiumImage')],create);
-    app.put('/stadium', [tokenVerfication], edit);
-    app.delete('/stadium/:id', _delete);
-}
+  app.get('/stadium', index);
+  app.get('/stadium/:id', show);
+  app.post(
+    '/stadium',
+    [tokenVerfication, adminAuthorization, upload.single('stadiumImage')],
+    create
+  );
+  app.put('/stadium', [tokenVerfication, adminAuthorization], edit);
+  app.delete('/stadium/:id', [tokenVerfication, adminAuthorization], _delete);
+};
 
 export default stadiumRoutes;
--- Front-end/src/Api/match/MatchController.js
+++ Front-end/src/Api/match/MatchController.js
@@ -1,44 +1,48 @@
 import axios from 'axios';
+import useAxiosPrivate from '../../Hooks/useAxiosPrivate';
 import { config } from '../../config';
 
 const MatchController = () => {
+  const axiosPrivate = useAxiosPrivate();
+
   async function remove(id) {
     try {
-      const response = await axios.delete(`${config.url}/match/${id}`);
+      const response = await axiosPrivate.delete(`${config.url}/match/${id}`);
+      return response;
     } catch (error) {}
   }
 
   async function add(data) {
     try {
-      const response = await axios.post(`${config.url}/match`, data);
+      const response = await axiosPrivate.post(`${config.url}/match`, data);
       console.log(response?.status);
     } catch (error) {}
   }
 
   async function getMatches() {
     try {
-      const response = await axios.get(`${config.url}/match`);
+      const response = await axiosPrivate.get(`${config.url}/match`);
       return response?.data;
     } catch (err) {}
   }
 
   async function getMatchesWithStadium() {
     try {
-      const response = await axios.get(`${config.url}/matchWithStadium`);
+      const response = await axiosPrivate.get(`${config.url}/matchWithStadium`);
       return response?.data;
     } catch (err) {}
   }
 
   async function getMatchById(id) {
     try {
-      const response = await axios.get(`${config.url}/match/${id}`);
+      const response = await axiosPrivate.get(`${config.url}/match/${id}`);
       return response?.data;
     } catch (err) {}
   }
 
   async function update(data) {
     try {
-      const response = await axios.put(`${config.url}/match`, data);
+      const response = await axiosPrivate.put(`${config.url}/match`, data);
     } catch (error) {}
   }
 
